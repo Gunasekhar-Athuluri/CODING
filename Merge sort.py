789c8c3ab3ae8c2e0cb90f7d695d109ab699fc01
Gunasekhar Athuluri <109656857+Gunasekhar-Athuluri@users.noreply.github.com>
2025-03-03 22:43:37 -0700

    Update Merge sort.py

--- Merge sort.py
+++ Merge sort.py
@@ -5,7 +5,7 @@ def mergeSort(array):
     if len(array) > 1:
 
         #  r is the point where the array is divided into two subarrays
-        r = len(array)//2
+        r = len(array) // 2
         L = array[:r]
         M = array[r:]
 
@@ -15,42 +15,41 @@ def mergeSort(array):
 
         i = j = k = 0
 
-        # Until we reach either end of either L or M, pick larger among
-        # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if L[i] < M[j]:
-                array[k] = L[i]
-                i += 1
-            else:
-                array[k] = M[j]
-                j += 1
-            k += 1
-
-        # When we run out of elements in either L or M,
-        # pick up the remaining elements and put in A[p..r]
-        while i < len(L):
+        def pick_left():
+            nonlocal k, i
             array[k] = L[i]
             i += 1
             k += 1
 
-        while j < len(M):
+        def pick_right():
+            nonlocal k, j
             array[k] = M[j]
             j += 1
             k += 1
 
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                pick_left()
+            else:
+                pick_right()
 
-# Print the array
-def printList(array):
-    for i in range(len(array)):
-        print(array[i], end=" ")
+        while i < len(L):
+            pick_left()
+
+        while j < len(M):
+            pick_right()
+
+
+def print_list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
     print()
 
 
-# Driver program
 if __name__ == '__main__':
-    array = [6, 5, 12, 10, 9, 1]
+    array = [6, 5, 12, 10, 45, 0, 4, 9, 1]
 
     mergeSort(array)
 
     print("Sorted array is: ")
-    printList(array)
+    print_list(array)
